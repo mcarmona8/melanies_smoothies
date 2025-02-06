34a72835daf885435e19863439f1bb56859a7d90
Michael Carmona <mcg1783@gmail.com>
2025-02-06 15:50:47 -0600

    Update streamlit_app.py
    Comment out lines

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,8 +32,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col("searc
 #st.dataframe(data=my_dataframe, use_container_width=True)
 # Convert the Snowpark DF to a Pandas DF so we can use the LOC function
 pd_df=my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+#st.dataframe(pd_df)
+#st.stop()
 
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients:",
